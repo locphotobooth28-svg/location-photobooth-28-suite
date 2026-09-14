2228ceb38bf3062a80bc414726b20885fc7d8ce2
locphotobooth28-svg <loc.photobooth.28@gmail.com>
2026-09-15 01:47:49 +0200

    Active l'amélioration documents organisateur

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { initOrganizerDocumentEnhancer } from "./organizerDocumentEnhancer";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+initOrganizerDocumentEnhancer();
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
