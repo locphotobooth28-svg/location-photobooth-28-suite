e9f660ecf87f26640883e04306aa7b48ba9e3210
locphotobooth28-svg <loc.photobooth.28@gmail.com>
2026-09-16 19:42:01 +0200

    Load LP28 mobile scroll fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "./mobile-scroll-fix.css";
 import { initOrganizerDocumentEnhancer } from "./organizerDocumentEnhancer";
 
 createRoot(document.getElementById("root")).render(
